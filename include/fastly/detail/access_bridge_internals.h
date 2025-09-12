4d0486e6d4697917cf5208a9273dd84bc14c602a
Sy Brand <sy.brand@fastly.com>
2025-09-12 16:48:03 +0100

    Comments

--- include/fastly/detail/access_bridge_internals.h
+++ include/fastly/detail/access_bridge_internals.h
@@ -3,26 +3,18 @@
 
 #include <fastly/sdk-sys.h>
 
-namespace fastly::detail
-{
-    struct AccessBridgeInternals
-    {
-        template <class T>
-        static auto &get(T &obj)
-        {
-            return obj.inner();
-        }
-        template <class T>
-        static auto &get(const T &obj)
-        {
-            return obj.inner();
-        }
-        template <class T, class U>
-        static auto from_raw(U *ptr)
-        {
-            return T(rust::Box<U>::from_raw(ptr));
-        }
-    };
-}
+namespace fastly::detail {
+// This type can be used to access the inner `rust::Box` of
+// various wrapper types in the C++ SDK.
+// It can also be used to construct wrapper types from raw pointers.
+// This is intended for internal use only.
+struct AccessBridgeInternals {
+  template <class T> static auto &get(T &obj) { return obj.inner(); }
+  template <class T> static auto &get(const T &obj) { return obj.inner(); }
+  template <class T, class U> static auto from_raw(U *ptr) {
+    return T(rust::Box<U>::from_raw(ptr));
+  }
+};
+} // namespace fastly::detail
 
 #endif
